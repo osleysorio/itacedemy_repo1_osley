b3171d4461f3f89a6aa4d725915afc93acb0bdb3
osleysorio <80639380+osleysorio@users.noreply.github.com>
2026-05-12 12:47:59 +0200

    Add files via upload

--- sprint 7/holamundo.py
+++ sprint 7/holamundo.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('hola mundo loco')
 
@@ -7,3 +9,14 @@ edad = st.number_input('Introduce tu edad:', min_value=0, max_value=120, value=2
 
 st.write(f'La edad ingresada es: {edad}')
 st.write(f'Tipo de dato: {type(edad)}')
+
+st.title("Mi primer gráfico en Streamlit")
+
+# Creamos datos ficticios: una tabla con 20 filas y 3 columnas
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['Ventas', 'Gastos', 'Beneficios']
+)
+
+# Mostramos el gráfico de líneas
+st.line_chart(chart_data)
