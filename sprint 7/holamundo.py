df47873f938eb55396868c6d1f5cc04bbd935a4b
osleysorio <80639380+osleysorio@users.noreply.github.com>
2026-05-12 12:24:02 +0200

    Add files via upload

--- sprint 7/holamundo.py
+++ sprint 7/holamundo.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.title('hola mundo loco')
 
-a = st.number_input ()
-b = st.number_input ()
+# Solicitar un entero con un paso de 1
+edad = st.number_input('Introduce tu edad:', min_value=0, max_value=120, value=25, step=1)
 
-st.title(a+b)
+st.write(f'La edad ingresada es: {edad}')
+st.write(f'Tipo de dato: {type(edad)}')
